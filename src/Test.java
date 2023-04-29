e721be183aed6be320070a842cc96e09af5208c9
JaeWon <bjw1622@gmail.com>
2023-04-29 18:16:47 +0900

    Add : Test.java

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args){
+        String[] test = {"콜라","맥주","소주"};
+        System.out.println(test[0]);
+        System.out.println("test");
+    }
+}
