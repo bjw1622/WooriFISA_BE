ec2d7547ae670952ac82d747f205dac4f7b3cda8
JaeWon <bjw1622@gmail.com>
2023-04-29 23:40:49 +0900

    Add : Test.java

---
+++ src/Example/SelfIntroduction.java
@@ -0,0 +1,9 @@
+package Example;
+
+public class SelfIntroduction {
+    public static void main(String[] args){
+        System.out.println("안녕하세요 반값습니다.");
+        System.out.println("저는 열혈 청년 백재원 입니다.");
+        System.out.println("잘 부탁드리겠습니다!");
+    }
+}
