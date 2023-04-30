d6155456e6e48f12f7251d295f9d1b4531106116
JaeWon <bjw1622@gmail.com>
2023-04-30 16:57:18 +0900

    Add : Pratice .java file

--- src/Example/Profile.java
+++ src/Example/Profile.java
@@ -2,6 +2,7 @@ package Example;
 
 public class Profile {
     public static void main(String[] args){
+
         String name = "백재원";
         float schoolNumber = 2017210103;
         double height = 1.78;
@@ -11,6 +12,6 @@ public class Profile {
         System.out.println("학번 : "+schoolNumber);
         System.out.println("신장 : "+height+"m");
         System.out.println("남자인가요? : "+isMan);
-
     }
 }
+
---
+++ src/Example/ShoppingList.java
@@ -0,0 +1,12 @@
+package Example;
+
+public class ShoppingList {
+    public static void main(String[] args){
+        String[] itemNames = {"한우 꽃등심","참치 선물팩","맥스봉 한팩"};
+        int[] itemPrices = {112500,25500,500};
+
+        System.out.println("품명: "+itemNames[0]+"  가격: " + itemPrices[0]);
+        System.out.printf("품명: %s  가격 : %d \n",itemNames[1],itemPrices[1]);
+        System.out.printf("품명: %s  가격 : %d \n",itemNames[2],itemPrices[2]);
+    }
+}
